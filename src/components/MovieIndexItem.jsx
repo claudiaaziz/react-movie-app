69b8bf8f04a6f43aad70c7ac70dcae6f27f2a1ea
claudiaaziz <claudiahannahh@gmail.com>
2024-03-23 21:52:41 -0400

    Api working

--- src/components/MovieIndexItem.jsx
+++ src/components/MovieIndexItem.jsx
@@ -1,9 +1,9 @@
 const MovieIndexItem = ({ movie }) => {
   return (
-    <div>
+    <div className="d-flex justify-content-start m-3">
       <img src={movie.Poster} alt={movie.Title} />
-      <h2>{movie.Title}</h2>
-      <p>{movie.Year}</p>
+      {/* <h2>{movie.Title}</h2> */}
+      {/* <p>{movie.Year}</p> */}
     </div>
   )
 }
